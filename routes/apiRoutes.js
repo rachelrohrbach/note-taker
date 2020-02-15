8c7e66488e4a944ea6bafde4039a72a7852a315e
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-02-14 20:59:07 -0800

    updated id generation to avoid duplicates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,8 @@ const notesData = JSON.parse(
     if (err) throw err;
   })
 );
-const newNotesData = notesData => {
+
+function writeNotesData(notesData) {
   fs.writeFileSync(
     path.join(__dirname, "../db/db.json"),
     JSON.stringify(notesData),
@@ -16,30 +17,45 @@ const newNotesData = notesData => {
       if (err) throw err;
     }
   );
-};
+}
+
+function getId(notesData) {
+  if(notesData.length === 0) {
+    return 0;
+  }
+
+  notesData.sort((a, b) => a.id - b.id);
+
+  const nextIndexAfterLast = notesData.length;
+  for(let i = 0; i < nextIndexAfterLast; i++) {
+    if(notesData[i].id !== i) {
+      return i;
+    }
+  }
+
+  return nextIndexAfterLast;
+}
 
 module.exports = app => {
   app.get("/api/notes", (req, res) => res.json(notesData));
 
   app.post("/api/notes", (req, res) => {
     let data = req.body;
-    let id = notesData.length;
-    data.id = id + 1;
+    data.id = getId(notesData);
     notesData.push(data);
-    newNotesData(notesData);
+    writeNotesData(notesData);
     return res.json(notesData);
   });
 
   app.delete("/api/notes/:id", (req, res) => {
     const id = parseInt(req.params.id);
     for (let i = 0; i < notesData.length; ++i) {
-      const note = notesData[i];
-      if (id === note.id) {
+      if (id === notesData[i].id) {
         notesData.splice(i, 1);
-        break;
+        return;
       }
     }
-    newNotesData(notesData);
+    writeNotesData(notesData);
     res.send(notesData);
   });
 };
