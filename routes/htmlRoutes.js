97fdc8bab62e2251021981d3be25e0d75fe60284
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-02-14 16:28:25 -0800

    added app.use static for using css

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,16 +3,9 @@
 const path = require("path");
 
 module.exports = app => {
-  app.get("/public/assets/css/styles.css", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/css/styles.css"));
-  });
 
-  app.get("/public/assets/js/index.js", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/js/index.js"));
-  });
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+  app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/index.html"))
   );
 
   app.get("/notes", (req, res) =>
