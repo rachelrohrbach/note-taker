bc9ae54b1ca1377075fbd92d5c00d1777d41595e
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-02-14 16:27:29 -0800

    added ids in post and created delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,17 +3,43 @@
 const fs = require("fs");
 const path = require("path");
 
-const dbNotes = require("../db/db.json");
+const notesData = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, data) => {
+    if (err) throw err;
+  })
+);
+const newNotesData = notesData => {
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(notesData),
+    err => {
+      if (err) throw err;
+    }
+  );
+};
 
 module.exports = app => {
-  app.get("/api/notes", (req, res) => res.json(dbNotes));
+  app.get("/api/notes", (req, res) => res.json(notesData));
 
   app.post("/api/notes", (req, res) => {
-    dbNotes.push(req.body);
+    let data = req.body;
+    let id = notesData.length;
+    data.id = id + 1;
+    notesData.push(data);
+    newNotesData(notesData);
+    return res.json(notesData);
+  });
 
-    fs.writeFile("./db/db.json", JSON.stringify(dbNotes), err => {
-      if (err) throw err;
-    });
-    res.json(true);
+  app.delete("/api/notes/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    for (let i = 0; i < notesData.length; ++i) {
+      const note = notesData[i];
+      if (id === note.id) {
+        notesData.splice(i, 1);
+        break;
+      }
+    }
+    newNotesData(notesData);
+    res.send(notesData);
   });
 };
